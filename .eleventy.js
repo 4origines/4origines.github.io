48ee6a9acd0b7cd496e67c6371126c53a4ad2f69
Shawn Thompson <plansmash@me.com>
2022-06-01 15:34:18 -0400

    Update site name

--- .eleventy.js
+++ .eleventy.js
@@ -14,8 +14,8 @@ console.log('pathPrefix is set to ...', pathPrefix);
 // see "eleventyConfig.addGlobalData("site", globalData);"" below
 // related: https://github.com/11ty/eleventy/issues/1641
 const globalSiteData = {
-  title: "originebrass.com",
-  description: "Website for the 4 Origines brewery in Dorval",
+  title: "Origine Brass - Restaurant",
+  description: "Website for the 4 Origines restaurant in Dorval",
   locale: 'en',
   baseUrl: baseUrl,
   pathPrefix: pathPrefix,
