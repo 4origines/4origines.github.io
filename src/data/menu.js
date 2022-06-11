e9b1e61e8db31c6e48609dda324864512f7fe513
Shawn Thompson <plansmash@me.com>
2022-06-11 12:05:57 -0400

    update menu

---
+++ src/data/menu.js
@@ -0,0 +1,328 @@
+module.exports = {
+  en: [
+    {
+      section: "Sharables / Starters",
+      description: "",
+      items: [
+        {
+          name: "Marinated Olives",
+          description: "Mixed olives, semi-dried tomatoes, lemon zest & garlic served with house focaccia",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: true,
+        },
+        {
+          name: "Fries or Tater Tots",
+          description: "Quebec potatoes or tater tots served with a spicy whipped feta dip",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Charcuterie Board",
+          description: "Charcuteries, Quebec cheese, pâté, olives, nuts, house focaccia",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Loaded Fries",
+          description: "Pork belly. cheese curds, fries, crispy onions and house beer aioli",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Cauliflower Tostada",
+          description:
+            "Crispy corn tortilla topped with charred cauliflower, pickled red onions, macha salsa, plant-based avocado crema and micro greens",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+        },
+        {
+          name: "German-Style Pretzel",
+          description: "Warm German-style pretzel served with house beer mustard",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Jalapeno Poppers",
+          description: "Jalapenos stuffed with cheese, shallots & apples. Breaded & fried",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+        },
+      ],
+    },
+    {
+      section: "Salads & Sandwiches",
+      description: "",
+      items: [
+        {
+          name: "House Salad",
+          description:
+            "Mixed greens, fresh herbs, cranberries, spiced nuts, cucumbers, maple- glazed smoked bacon, crispy onions, parmesan and apple cider vinaigrette",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Corn Salad",
+          description:
+            "Corn, red cabbage, hierloom tomatoes, cucumbers, mangoes, coriander, feta, avocado and sourdough croutons tossed in a chili lime vinaigrette",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Caprese Salad",
+          description:
+            "Peaches, hierloom tomatoes, fiore di latte, oat rice, micro greens and a passion fruit vinaigrette",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Scallop & Shrimp Salad",
+          description:
+            "Bay scallops, shrimps, red onions, cucumbers, radishes, wild greens & heirloom tomatoes tossed in a beer, habanero and mango vinaigrette",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Deli Sandwich",
+          description:
+            "Genoa, mortadelle, coppa, fiore di latte, hot peppers, pickles, mixed greens, tomatoes and pesto served on sourdough bread. Served with fries",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Grilled Cheese Sandwich",
+          description:
+            "Braised short ribs, sea buckthorn jelly, aged gouda, mozzarella on sourdough bread. Served with fries",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      section: "Pizza",
+      description: "Our sourdough is made in-house & left to rise over 48 hours.",
+      items: [
+        {
+          name: "Marinara",
+          description: "Tomato sauce, basil, olive oil",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Margherita",
+          description: "Tomato sauce, fresh mozzarella, basil and olive oil",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Artichoke & Goat Cheese",
+          description:
+            "Sundried tomato pesto, fried artichokes, pumkin seeds, goat cheese and wild greens drizzled with beer and honey reduction",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Sausage",
+          description: "Smoked sausage, parmesan cream sauce, Quebec asparagus, leeks",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Deli",
+          description: "Tomato sauce, genoa, mortadelle, capicollo, provolone & mozzarella",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+        {
+          name: "Mushroom",
+          description:
+            "Local mushrooms, spinach, semi-dried tomatoes, garlic confit with ricotta and grana padano cheese.",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: false,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      section: "Mains",
+      description: "",
+      items: [
+        {
+          name: "Origine Brass Burger",
+          description:
+            "8-oz beef patty with bacon, Monteray Jack cheese, chive & garlic confit mayo, pickles, tomatoes and lettuce. Served with fries",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+        },
+        {
+          name: "Debrenzener Hot Dog",
+          description:
+            "Hungarian smoked sausage in a soft bun topped with pickled mango and cabbage slaw. Served with fries",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+        },
+        {
+          name: "Fried Chicken And Waffles",
+          description: "Crispy house fried chicken with a sesame waffle & a citrus chili & maple reduction",
+          image: "menu-item.png",
+          Vegetarian: true,
+          Vegan: true,
+          GlutenFree: false,
+          additions: [
+            {
+              name: "Tofu",
+            },
+          ],
+        },
+        {
+          name: "House-Cut Rib Eye Filet",
+          description:
+            "Pan-seared rib eye filet served with (sauce to create) a sweet potato stuffed with n'duja, greek yogurt, chives and crumbled feta",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+      ],
+    },
+    {
+      section: "Kids' Corner",
+      description: "",
+      items: [
+        {
+          name: "Crispy Chicken And Tater Tots",
+          description: "House-fried chicken and crispy tater tots",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Burger",
+          description: "4-oz patty, cheese and bacon on brioche bun served with fries",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Hot Dog",
+          description: "Large beef sausage served with fries",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+        {
+          name: "Cheese Pizza",
+          description: "Tomato sauce, mozzarella cheese",
+          image: "menu-item.png",
+          Vegetarian: false,
+          Vegan: false,
+          GlutenFree: false,
+        },
+      ],
+    },
+  ],
+  fr: {},
+};
